393966015d1e04c9456a743525a1c8cc14d3730c
iawavgvst <alkelin1@gmail.com>
2025-01-22 20:27:11 +0300

    controller (+routing)

---
+++ app/Http/Requests/Task/StoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Task;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'due_date' => 'required|date',
+            'status' => 'required|in:выполнена,не выполнена',
+            'priority' => 'required|in:низкий,средний,высокий',
+            'category' => 'required|in:работа,дом,личное',
+        ];
+    }
+}
